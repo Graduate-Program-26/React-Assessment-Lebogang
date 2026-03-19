8bbfa56d036a88a6677209099386fd44e5b44c29
LebogangMasenya <01@01January2026.com>
2026-03-19 19:44:18 +0200

    feat: added structre to themeing and navrab components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { ThemeProvider } from "@/components/theme-provider";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -24,10 +24,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      ><ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
       >
-        {children}
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- components/naviagtion/NavBar.tsx
+++ components/naviagtion/NavBar.tsx
@@ -1,11 +1,34 @@
 
+'use client'
+import * as React from "react"
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
+import { Button } from "../ui/button"
+
 export default function Navbar() {
+    const { setTheme, systemTheme } = useTheme();
+
+    function handleThemeToggle() {
+        if (systemTheme === "dark") {
+            setTheme("light")
+        } else {
+            setTheme("dark")
+        }
+    }
+
+
     return (
         <nav className="bg-gray-800 p-4">
             <div className="container mx-auto flex items-center justify-between">
                 <div className="text-white text-lg font-bold">GitGram</div>
                 <div className="flex space-x-4">
                     <a href="#" className="text-gray-300 hover:text-white">Notifications</a>
+                    
+                    <Button variant="outline" size="icon" onClick={handleThemeToggle}>
+                        <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+                        <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+                        <span className="sr-only">Toggle theme</span>
+                    </Button>
                 </div>
             </div>
         </nav>
