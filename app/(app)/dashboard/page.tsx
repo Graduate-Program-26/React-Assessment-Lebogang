28885bf72f9bc70a89e51eb1689855e3f18c7071
LebogangMasenya <01@01January2026.com>
2026-03-23 00:24:13 +0200

    feat: cleaned up search bars

--- app/(app)/dashboard/page.tsx
+++ app/(app)/dashboard/page.tsx
@@ -1,16 +1,19 @@
+
 import StoriesRow from "./_components/stories_row/StoriesRow";
-import SearchBar from "@/components/shared/SearchBar";
 import Feed from "./_components/feed/Feed";
 import SuggestedDevs from "./_components/suggested_devs/SuggestedDevs";
 
+
+
+
 export default function Home() {
+
   return (
     <div className="w-full max-w-7.5xl mx-auto px-4">
 
       {/* mobile-only top elements */}
       <div className="md:hidden">
         <StoriesRow />
-        <SearchBar />
       </div>
 
       <div className="flex gap-8">
@@ -26,7 +29,6 @@ export default function Home() {
         {/* right col —  hidden on mobile */}
         <aside className="hidden md:block w-100">
           <div className="sticky top-3 flex flex-col gap-6">
-            <div className="mt-4">   <SearchBar /> </div>
             <SuggestedDevs />
           </div>
         </aside>
