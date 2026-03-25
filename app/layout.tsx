53a139076ca5d5e807a04c120bb22616fffd0946
LebogangMasenya <01@01January2026.com>
2026-03-25 14:17:00 +0200

    fix: sorted out hydration issue on `layout.tsx` due to conflicts with the theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,21 @@
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import { SessionProvider } from "next-auth/react";
-import { auth } from "@/lib/auth";
 import { Toaster } from "@/components/ui/sonner";
 export default async function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
 
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className="bg-background text-foreground font-sans">
-   
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider>
-  
+
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem={true}
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+
         <Toaster />
       </body>
     </html>
