a46439029b8dddfb6048571d07ceb4ecc2755a50
LebogangMasenya <01@01January2026.com>
2026-03-26 12:50:23 +0200

    refactor: improved handling of search bar text wuith debounce

--- components/shared/SearchBar.tsx
+++ components/shared/SearchBar.tsx
@@ -6,9 +6,8 @@ import {useDebounce} from "use-debounce"
 import { useState, useEffect } from 'react'
 
 export default function SearchBar(values: {value: string, onChange: (value: string) => void, placeholder?: string}) {
-    const [query] = useDebounce(values.value, 500);
-    const [text, setText] = useState(values.value)
-
+    const [text, setText] = useState("");
+    const [query] = useDebounce(text, 500);
     useEffect(() => {
        // This prevents searching for empty strings on mount
         if (query !== undefined) {
